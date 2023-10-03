658c43cae63be4deba3d8252b7a64654731bc0d9
Jason Sun <sunapi386@gmail.com>
2023-10-03 14:55:27 +0000

    my first git commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        <img src="your-profile-picture.jpg" className="App-logo" alt="logo" />
+        <h1>Your Name</h1>
+        <p>Software Developer | React Enthusiast</p>
+        <ul className="nav">
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#resume">Resume</a></li>
+        </ul>
       </header>
+      <main className="App-content">
+        <section id="projects">
+          <h2>My Projects</h2>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <h3>Project 1</h3>
+              <p>Description of project 1 goes here</p>
+              <img src="project1.png" alt="Project 1 screenshot" />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <h3>Project 2</h3>
+              <p>Description of project 2 goes here</p>
+              <img src="project2.png" alt="Project 2 screenshot" />
+            </Grid>
+            // Add more projects here
+          </Grid>
+        </section>
+        <section id="resume">
+          <h2>My Resume</h2>
+          <pdf
+            src="your-resume.pdf"
+            style={{ width: '100%', height: '100vh' }}
+            page={1}
+          />
+        </section>
+      </main>
     </div>
   );
 }
---
+++ src/components/MyBackgroundInfo.js
@@ -0,0 +1,22 @@
+import '../App.css';
+
+function BackgroundInfo() {
+  return (
+    <div className="BackgroundInfo">
+      <header className="BackgroundInfo-header">
+        
+        <p>
+          Phil's Website <span className="heart">♥️</span> React
+        </p>
+        <p className="small">
+          Edit <code>src/BackgroundInfo.js</code> and save to reload.
+        </p>
+
+        
+       
+      </header>
+    </div>
+  );
+}
+
+export default BackgroundInfo;
